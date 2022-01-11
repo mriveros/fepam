d22f84b592ed2a0a74fa0dbff1430441c70992f1
Marcos R <mriveros.py@gmail.com>
2022-01-10 23:42:24 -0300

    modified:   app/models/torneo.rb 	modified:   app/views/torneos/_agregar.html.erb 	modified:   app/views/torneos/_editar.html.erb 	modified:   log/development.log

--- app/models/torneo.rb
+++ app/models/torneo.rb
@@ -3,7 +3,7 @@ class Torneo < ActiveRecord::Base
   self.table_name= "torneos"
   self.primary_key = "id"
   
-  attr_accessible :id, :descripcion, :cantidad_fechas, :created_at, :updated_at
+  attr_accessible :id, :descripcion, :cantidad_fechas, :fecha, :created_at, :updated_at
  
   scope :orden_01, -> { order("id")}
   scope :orden_descripcion, -> { order("descripcion")}
