7fb92025d5fb75eaeedd9511fb934cb525d52904
Marcos R <mriveros.py@gmail.com>
2022-01-17 22:37:47 -0300

    Changes to be committed: 	renamed:    app/controllers/clientes_controller.rb -> app/controllers/pilotos_controller.rb 	new file:   app/models/piloto.rb 	renamed:    app/views/clientes/_agregar.html.erb -> app/views/pilotos/_agregar.html.erb 	renamed:    app/views/clientes/_editar.html.erb -> app/views/pilotos/_editar.html.erb 	renamed:    app/views/clientes/_link_to_editar_cliente.html.erb -> app/views/pilotos/_link_to_editar_cliente.html.erb 	renamed:    app/views/clientes/_lista.html.erb -> app/views/pilotos/_lista.html.erb 	renamed:    app/views/clientes/actualizar.js.erb -> app/views/pilotos/actualizar.js.erb 	renamed:    app/views/clientes/agregar.js.erb -> app/views/pilotos/agregar.js.erb 	renamed:    app/views/clientes/editar.js.erb -> app/views/pilotos/editar.js.erb 	renamed:    app/views/clientes/eliminar.js.erb -> app/views/pilotos/eliminar.js.erb 	renamed:    app/views/clientes/guardar.js.erb -> app/views/pilotos/guardar.js.erb 	renamed:    app/views/clientes/index.html.erb -> app/views/pilotos/index.html.erb 	renamed:    app/views/clientes/lista.js.erb -> app/views/pilotos/lista.js.erb 	modified:   app/views/torneos/guardar_torneo_detalle.js.erb

---
+++ app/models/piloto.rb
@@ -0,0 +1,11 @@
+class Piloto < ActiveRecord::Base
+
+  self.table_name="pilotos"
+  self.primary_key="id"
+  
+  attr_accessible :id, :nombres, :apellidos, :ci, :grupo_sanguineo, :direccion, :telefono, :fecha_nacimiento
+  
+  scope :orden_01, -> { order("id")}
+  scope :orden_descripcion, -> { order("nombres, apellidos")}
+  
+end
