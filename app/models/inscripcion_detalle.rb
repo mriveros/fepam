41bb6b0789177ac0f0859b769e37b5dfbe9858a2
Marcos R <mriveros.py@gmail.com>
2022-02-17 23:23:23 -0300

    modified:   app/models/inscripcion_detalle.rb

--- app/models/inscripcion_detalle.rb
+++ app/models/inscripcion_detalle.rb
@@ -3,7 +3,7 @@ class InscripcionDetalle < ActiveRecord::Base
   self.table_name= "inscripciones_detalles"
   self.primary_key = "id"
   
-  attr_accessible :id, :piloto_id, :fecha_inscripcion, :created_at, :updated_at
+  attr_accessible :id, :piloto_id, :fecha_inscripcion, :precio_id, :estado_inscripcion_id, :created_at, :updated_at
  
   scope :orden_01, -> { order("id")}
   scope :orden_descripcion, -> { order("descripcion")}
