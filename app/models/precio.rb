efb5c577da3fb9f75733dc348a22a8e3e5a7651b
Marcos R <mriveros.py@gmail.com>
2022-03-03 23:48:15 -0300

    renamed:    app/models/estado_celo.rb -> app/models/precio.rb 	modified:   app/views/inscripciones/_agregar_inscripcion_detalle.html.erb

--- app/models/precio.rb
+++ app/models/precio.rb
@@ -1,6 +1,6 @@
-class EstadoCelo < ActiveRecord::Base
+class Precio < ActiveRecord::Base
   
-  self.table_name= "estados_celos"
+  self.table_name= "precios"
   self.primary_key = "id"
   
   attr_accessible :id, :descripcion, :created_at, :updated_at
