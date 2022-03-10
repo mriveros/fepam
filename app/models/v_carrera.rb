0c1f08a9e40f3e849e1ffd5e9f327ffd1c53f505
Marcos R <mriveros.py@gmail.com>
2022-03-09 22:45:31 -0300

    modified:   app/models/v_carrera.rb 	renamed:    app/views/carreras/_agregar_inscripcion_detalle.html.erb -> app/views/carreras/_agregar_carrera_detalle.html.erb 	renamed:    app/views/carreras/_inscripcion_detalle.html.erb -> app/views/carreras/_carrera_detalle.html.erb 	renamed:    app/views/carreras/_link_to_inscripcion_detalle.html.erb -> app/views/carreras/_link_to_carrera_detalle.html.erb 	renamed:    app/views/carreras/_link_to_editar_inscripcion.html.erb -> app/views/carreras/_link_to_editar_carrera.html.erb 	modified:   app/views/carreras/_lista.html.erb 	modified:   app/views/carreras/index.html.erb 	modified:   app/views/carreras/lista.js.erb 	modified:   app/views/layouts/_menu.html.erb

--- app/models/v_carrera.rb
+++ app/models/v_carrera.rb
@@ -3,9 +3,9 @@ class VCarrera < ActiveRecord::Base
   self.table_name= "v_carreras"
   self.primary_key = "carrera_id"
   
-  attr_accessible :carrera_id, :inscripcion_id, :torneo_id, :torneo, :torneo_detalle_id, :torneo_detalle, :categoria_id, :categoria, :created_at, :updated_at
+  attr_accessible :carrera_id,:inscripcion_id,:fecha_carrera,:torneo,:fecha,:categoria_id,:categoria,:estado_carrera_id,:estado_carrera,:created_at,:updated_at
  
   scope :orden_01, -> { order("carrera_id")}
-  scope :orden_torneo_id, -> { order("orden_torneo_id desc")}
+  scope :orden_fecha, -> { order("created_at desc")}
 
 end
