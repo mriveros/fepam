b1acc0e6360436644a78c522350f4106c50a0eaa
Marcos R <mriveros.py@gmail.com>
2022-03-12 14:16:26 -0300

    new file:   app/helpers/carreras_helper.rb 	modified:   app/views/carreras/_carrera_detalle.html.erb 	modified:   app/views/carreras/_link_to_carrera_detalle.html.erb 	modified:   app/views/carreras/_lista.html.erb 	renamed:    app/views/carreras/agregar_inscripcion_detalle.js.erb -> app/views/carreras/agregar_carrera_detalle.js.erb 	new file:   app/views/carreras/carrera_detalle.js.erb 	renamed:    app/views/carreras/eliminar_inscripcion_detalle.js.erb -> app/views/carreras/eliminar_carrera_detalle.js.erb 	renamed:    app/views/carreras/guardar_inscripcion_detalle.js.erb -> app/views/carreras/guardar_carrera_detalle.js.erb 	deleted:    app/views/carreras/inscripcion_detalle.js.erb

---
+++ app/helpers/carreras_helper.rb
@@ -0,0 +1,16 @@
+module CarrerasHelper
+
+  def link_to_editar_carrera(carrera)
+
+      render partial: 'link_to_editar_carrera', locals: { carrera: carrera }
+      
+  end
+
+  def link_to_carrera_detalle(carrera_id)
+
+      render partial: 'link_to_carrera_detalle', locals: { carrera_id: carrera_id }
+      
+  end
+
+  
+end
