9b150bc7cd904c6c92c4dd52f6ef05413d14a608
Marcos R <mriveros.py@gmail.com>
2022-03-15 20:17:56 -0300

    modified:   app/models/v_carrera_detalle.rb 	modified:   app/views/carreras/_carrera_detalle.html.erb 	modified:   app/views/carreras/_link_to_carrera_detalle.html.erb

--- app/models/v_carrera_detalle.rb
+++ app/models/v_carrera_detalle.rb
@@ -3,7 +3,7 @@ class VCarreraDetalle < ActiveRecord::Base
   self.table_name= "v_carreras_detalles"
   self.primary_key = "carrera_detalle_id"
   
-  attr_accessible :carrera_detalle_id,:carrera_id,:piloto_id, :created_at,:updated_at
+  attr_accessible :carrera_detalle_id,:carrera_id,:piloto_id, :nombre_piloto, :posicion,  :created_at,:updated_at
  
   scope :orden_01, -> { order("carrera_detalle_id")}
   scope :orden_fecha, -> { order("posicion desc")}
