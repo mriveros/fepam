601435d2224154e9ee96a24e46e7684392a3ceba
Marcos R <mriveros.py@gmail.com>
2022-03-22 17:13:57 -0300

    modified:   app/models/v_carrera_tiempo.rb 	modified:   app/views/carreras/_carrera_detalle.html.erb

--- app/models/v_carrera_tiempo.rb
+++ app/models/v_carrera_tiempo.rb
@@ -3,7 +3,7 @@ class VCarreraTiempo < ActiveRecord::Base
   self.table_name= "v_carreras_tiempos"
   self.primary_key = "piloto_id"
   
-  attr_accessible :piloto_id,:tiempos, :carrera_id, :max
+  attr_accessible :piloto_id,:tiempos, :carrera_id, :cantidad_vueltas, :posicion
  
   scope :orden_piloto, -> { order("piloto_id desc")}
   scope :orden_tiempo, -> { order("tiempos desc")}
