56a165e238aa022ae87721601a2b4f82567e8e30
Marcos R <mriveros.py@gmail.com>
2022-03-28 21:36:24 -0400

    modified:   app/models/precio.rb 	modified:   app/views/inscripciones/_agregar_inscripcion_detalle.html.erb

--- app/models/precio.rb
+++ app/models/precio.rb
@@ -3,9 +3,11 @@ class Precio < ActiveRecord::Base
   self.table_name= "precios"
   self.primary_key = "id"
   
-  attr_accessible :id, :descripcion, :created_at, :updated_at
+  attr_accessible :id, :descripcion, :monto, :created_at, :updated_at
  
   scope :orden_01, -> { order("id")}
+
+  scope :monto_descripcion, -> { select("descripcion || ' ' || 'Gs.' || monto as descripcion")}
 
   
 end
