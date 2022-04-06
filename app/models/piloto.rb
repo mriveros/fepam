92668023f8c081039ab39506c732487daebf6c09
Marcos R <mriveros.py@gmail.com>
2022-04-05 23:08:46 -0400

    modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/models/piloto.rb 	modified:   app/views/pilotos/_agregar.html.erb 	new file:   config/initializers/dragonfly.rb

--- app/models/piloto.rb
+++ app/models/piloto.rb
@@ -2,8 +2,9 @@ class Piloto < ActiveRecord::Base
 
   self.table_name="pilotos"
   self.primary_key="id"
-  
-  attr_accessible :id, :nombres, :apellidos, :ci, :grupo_sanguineo_id, :direccion, :telefono, :fecha_nacimiento
+  include Gravtastic
+  gravtastic
+  attr_accessible :id, :nombres, :apellidos, :ci, :grupo_sanguineo_id, :direccion, :telefono, :fecha_nacimiento, :avatar_url
   
   scope :orden_01, -> { order("id")}
   scope :orden_descripcion, -> { order("nombres, apellidos")}
